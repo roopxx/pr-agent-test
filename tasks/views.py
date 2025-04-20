93ac8379e2d4a12abb47a07dbc89377e94529896
roopxx <127219908+roopxx@users.noreply.github.com>
2025-04-20 22:37:34 +0530

    test: new workflow

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Task
+from django.utils import timezone
 
 class TaskListView(ListView):
     model = Task
@@ -30,3 +31,26 @@ class TaskDeleteView(DeleteView):
     model = Task
     template_name = 'tasks/task_confirm_delete.html'
     success_url = reverse_lazy('task-list')
+
+def mark_task_completed(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.status = 'Completed'
+    task.save()
+    return redirect('task-list')
+
+def task_search(request):
+    query = request.GET.get('query', '')
+    if query:
+        tasks = Task.objects.filter(title__icontains=query) | Task.objects.filter(description__icontains=query)
+    else:
+        tasks = Task.objects.all().order_by('-created_date')
+    
+    return render(request, 'tasks/task_search.html', {'tasks': tasks, 'query': query})
+
+def tasks_by_status(request, status):
+    tasks = Task.objects.filter(status=status).order_by('-created_date')
+    return render(request, 'tasks/task_list.html', {'tasks': tasks, 'status': status})
+
+def overdue_tasks(request):
+    tasks = Task.objects.filter(due_date__lt=timezone.now(), status__not='Completed').order_by('due_date')
+    return render(request, 'tasks/task_list.html', {'tasks': tasks, 'list_title': 'Overdue Tasks'})
