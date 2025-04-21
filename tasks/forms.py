13da0216a6adb39382e4b422456dd9720dc8ef25
roopxx <127219908+roopxx@users.noreply.github.com>
2025-04-21 09:14:56 +0530

    feat: implement forms

---
+++ tasks/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'due_date', 'status', 'priority']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['due_date'].widget = forms.DateTimeInput(attrs={'type': 'datetime-local'})
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
