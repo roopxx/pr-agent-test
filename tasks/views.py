5c8ec41bab8b76e616f3ca0147b21bfc07c61755
roopxx <127219908+roopxx@users.noreply.github.com>
2025-04-21 08:52:09 +0530

    feat: implement basic task views

---
+++ tasks/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
+from .forms import TaskForm
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
+    ordering = ['-created_date']
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task-list')
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task-list')
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    success_url = reverse_lazy('task-list')
+
+def mark_as_complete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.status = 'completed'
+    task.save()
+    return redirect('task-list')
